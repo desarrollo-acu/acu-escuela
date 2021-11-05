c9f842e75aa02c1b8804d6cca94d5d75e49f22c0
Nahuel Bentos <nahuel.bentos@fing.edu.uy>
2021-11-04 22:55:30 -0300

    Fin backoffice formularios

--- src/app/core/services/reportes.service.ts
+++ src/app/core/services/reportes.service.ts
@@ -4,7 +4,7 @@ import { environment } from '@environments/environment';
 import { formatDateToString } from '@utils/utils-functions';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportesService {
   headers = new HttpHeaders();
@@ -12,31 +12,45 @@ export class ReportesService {
   options;
 
   constructor(private http: HttpClient) {
-
     this.headers.set('Aceppt', 'application/pdf;');
     this.options = {
       headers: this.headers,
       responseType: 'blob' as 'json',
-    }
-
-
-
+    };
   }
 
-  ocupacionInstructorPorPeriodo = () => {}
+  ocupacionInstructorPorPeriodo = () => {};
 
-  ocupacionInstructoresPorPeriodo = () => {}
+  ocupacionInstructoresPorPeriodo = () => {};
 
-  facturacionPorItem = (codigoInicial, codigoFinal, fechaInicio, fechaFinal) => this.http.post(`${environment.url_ws}/RListCo1`,{codigoInicial, codigoFinal, fechaInicio, fechaFinal}, this.options);
+  facturacionPorItem = (codigoInicial, codigoFinal, fechaInicio, fechaFinal) =>
+    this.http.post(
+      `${environment.url_ws}/RListCo1`,
+      { codigoInicial, codigoFinal, fechaInicio, fechaFinal },
+      this.options
+    );
 
   dataIniciosCursosPorPeriodo = (fechaInicial, fechaFinal) =>
-    this.http.get(`${environment.url_ws}/getDataInicioCurso?fechaInicial=${formatDateToString(fechaInicial.toDate())}&fechaFinal=${formatDateToString(fechaFinal.toDate())}`);
+    this.http.get(
+      `${
+        environment.url_ws
+      }/getDataInicioCurso?fechaInicial=${formatDateToString(
+        fechaInicial.toDate()
+      )}&fechaFinal=${formatDateToString(fechaFinal.toDate())}`
+    );
 
-  iniciosCursosPorPeriodo = (fechaInicial, fechaFinal) => this.http.post(`${environment.url_ws}/getExcelInicioCurso`,{fechaInicial, fechaFinal});
+  iniciosCursosPorPeriodo = (fechaInicial, fechaFinal) =>
+    this.http.post(`${environment.url_ws}/getExcelInicioCurso`, {
+      fechaInicial,
+      fechaFinal,
+    });
 
-  cantidadClasesPorPeriodo =  (  usrId, mes, anio) => this.http.post(`${environment.url_ws}/PEscConClases`,{ usrId, mes, anio});
+  cantidadClasesPorPeriodo = (usrId, mes, anio) =>
+    this.http.post(`${environment.url_ws}/PEscConClases`, { usrId, mes, anio });
   // REscClasesAlumHora
-  alumnosEnCurso = (  fechaDesde, fechaHasta) => this.http.post(`${environment.url_ws}/REscClasesAlumHora`,{ fechaDesde, fechaHasta});
-
-
+  alumnosEnCurso = (fechaDesde, fechaHasta) =>
+    this.http.post(`${environment.url_ws}/REscClasesAlumHora`, {
+      fechaDesde,
+      fechaHasta,
+    });
 }
